bdc4e69caaa795cbe0343fa798affdab571f9bac
wassim-oueslati <oueslatiiwassim@gmailcom>
2021-08-28 02:01:53 +0100

    new update

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,32 +1,84 @@
-var buttonsPlus = Array.from(document.getElementsByClassName("add"));
+var total=0;
 
+//add button
+var buttonsPlus = document.getElementsByClassName("add");
 
 for (let btnPlus of buttonsPlus) {
-  btnPlus.addEventListener("click", function () {
+    btnPlus.addEventListener("click", function () {
     btnPlus.nextElementSibling.innerHTML++;
-   
-  });
-}
 
-var buttonsMinus = Array.from(document.getElementsByClassName("minus"));
-
-for (let i = 0; i < buttonsPlus.length; i++) {
-  buttonsMinus[i].addEventListener("click", function () {
-    if (buttonsMinus[i].previousElementSibling.innerText > 0)
-      buttonsMinus[i].previousElementSibling.innerText--;
+    let quantity=btnPlus.parentNode.children[5].innerHTML;
+    quantity=parseFloat(quantity);
+    
+    let pricee=btnPlus.parentNode.children[7].children[0].innerHTML;
+    pricee=parseFloat(pricee);
+        
+    let subtotal = pricee * quantity;   
+    btnPlus.parentNode.children[8].children[0].innerText=subtotal;
+        
+        
+    document.getElementById("total-basket").innerHTML=calcTotal();
     
   });
 }
 
+//Reduce button
+var buttonsMinus = document.getElementsByClassName("minus");
 
-let trash = document.getElementsByClassName("fa-trash-alt");
-
-for (let btnTrash of trash) {
-  btnTrash.addEventListener("click", function () {
-    btnTrash.parentNode.remove();
+for (let btnMinus of buttonsMinus) {
+    btnMinus.addEventListener("click", function () {
+    if (btnMinus.previousElementSibling.innerText > 0)
+      btnMinus.previousElementSibling.innerText--;
+      
+      let quantity=btnMinus.parentNode.children[5].innerText;
+      quantity=parseFloat(quantity);
+    
+      let pricee=btnMinus.parentNode.children[7].children[0].innerText;
+      pricee=parseFloat(pricee);
+        
+      let subtotal = pricee*quantity;
+      btnMinus.parentNode.children[8].children[0].innerText=subtotal;
+        
+      document.getElementById("total-basket").innerHTML=calcTotal();
+      
+      
   });
 }
 
+//Delete button
+let trash = document.getElementsByClassName("fa-trash-alt");
+
+for (let btnTrash of trash) {
+    btnTrash.addEventListener("click", function () {
+    btnTrash.parentNode.remove();
+    
+    let quantity=btnTrash.parentNode.children[5].innerText;
+    quantity=parseFloat(quantity);
+  
+    let pricee=btnTrash.parentNode.children[7].children[0].innerText;
+    pricee=parseFloat(pricee);
+      
+    let subtotal = pricee*quantity;
+    btnTrash.parentNode.children[8].children[0].innerText=subtotal;
+      
+    document.getElementById("total-basket").innerHTML=calcTotal();
+    
+  });
+}
+
+//Basket total
+function calcTotal(){
+  let tot=document.getElementsByClassName("subtotal");
+  let total=0;
+  for (let element of tot){
+      total+=parseFloat(element.innerText);
+  }
+  return total;
+}
+
+
+
+//Heart button
 let hearts = document.getElementsByClassName("fa-heart");
 
 for (let heart of hearts) {
@@ -40,4 +92,8 @@ for (let heart of hearts) {
     }
   });
 }
+
+   
+
+
 
